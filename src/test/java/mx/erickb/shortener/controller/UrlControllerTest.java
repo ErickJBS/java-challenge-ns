6343228ee8e7f71cb6924527b5b4f75dcd487074
ErickJBS <a311008@uach.mx>
2020-07-27 01:44:30 -0600

    Junit4 Tests replaced and README updated

--- src/test/java/mx/erickb/shortener/controller/UrlControllerTest.java
+++ src/test/java/mx/erickb/shortener/controller/UrlControllerTest.java
@@ -1,15 +1,36 @@
 package mx.erickb.shortener.controller;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import mx.erickb.shortener.model.UrlRequest;
+import mx.erickb.shortener.service.UrlService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+
 @SpringBootTest
 public class UrlControllerTest {
 
     @Autowired
+    @InjectMocks
     private UrlController controller;
+
+    @Mock
+    private UrlService service;
+
+    @Test
+    public void saveUrlShouldReturnAlias() {
+        Mockito.when(
+                service.saveUrl(anyString())
+        ).thenReturn("mockAlias");
+
+        UrlRequest request = new UrlRequest("http://google.com/");
+        String alias = controller.saveUrl(request);
+
+        assertNotNull(alias);
+    }
 }
--- src/test/java/mx/erickb/shortener/service/UrlServiceTest.java
+++ src/test/java/mx/erickb/shortener/service/UrlServiceTest.java
@@ -1,14 +1,11 @@
 package mx.erickb.shortener.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class UrlServiceTest {
 
